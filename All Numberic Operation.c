648eae261a2981e0224326668e26ac809147d44c
Yash-Beldar <yashodip2003beldar@gmail.com>
2022-08-09 19:02:54 +0530

    Added main menu of program

---
+++ All Numberic Operation.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+void main()
+{
+    char menu;
+    printf("-----**This is program to do All Numeric Operation-----**\n");
+    printf("**Enter F1 to go in Main MENU****\n");
+    printf("**Enter ESC to Quit Program****\n");
+    scanf(" %c",&menu);
+    if(menu==17)
+      printf("**Welcome to Main MENU**");
+}
